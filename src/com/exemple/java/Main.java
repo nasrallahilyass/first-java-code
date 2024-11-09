1c3a76dc48dae280a751a19db696d600b13922e5
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-09 19:50:24 +0100

    Booleans

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -1,37 +1,23 @@
 package com.exemple.java;
 
 public class Main {
+    static boolean anonymous;
     public static void main(String[] args) {
-        System.out.println("Math Classes:");
+        System.out.println("Boolean Type:");
 
-        int x = 36;
-        int y = 20;
 
-        int add = x + y;
-        int sub = x - y;
-        int mul = x * y;
-        double div = (double) x / y;
-        int mod = x % y;
+        boolean bool1 = true;
+        System.out.println("Boolean is: " + bool1);
 
-        double negative = -98.887;
-        double absNegative = Math.abs(negative);
+        boolean bool2 = false;
+        System.out.println("Boolean is: " + bool2);
 
-        long rounded = Math.round(absNegative);
+        System.out.println("Boolean Type: " + anonymous);
 
-        double roundedCeil = Math.ceil(absNegative);
-        double roundedFloor = Math.floor(absNegative);
+        String sBoolean = "false";
+        boolean bBoolean = Boolean.parseBoolean(sBoolean);
+        System.out.println("convert boolean from string to boolean type: " + bBoolean);
 
-        System.out.println("Addition: " + add);
-        System.out.println("Subtraction: " + sub);
-        System.out.println("Multiplication: " + mul);
-        System.out.println("Division: " + div);
-        System.out.println("Modulus: " + mod);
-        System.out.println("###############################");
-        System.out.println("Negative: " + negative);
-        System.out.println("AbsNegative: " + absNegative);
-        System.out.println("###############################");
-        System.out.println("Rounded: " + rounded);
-        System.out.println("RoundedCeil: " + roundedCeil);
-        System.out.println("RoundedFloor: " + roundedFloor);
+
     }
 }
