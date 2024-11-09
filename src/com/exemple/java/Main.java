e249c12437e781d2a94d6a5c887a46a9c54c401b
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-09 18:35:40 +0100

    Numeric Data Types && Helper Classes

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -2,6 +2,24 @@ package com.exemple.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Helper Classes:");
+        System.out.println("Numeric Data Types:");
+        System.out.println("Min value Byte " + Byte.MIN_VALUE);
+        System.out.println("Max value Byte " + Byte.MAX_VALUE);
+        System.out.println("#################################");
+        System.out.println("Min value Short " + Short.MIN_VALUE);
+        System.out.println("Max value Short " + Short.MAX_VALUE);
+        System.out.println("################################");
+        System.out.println("Min value Integer " + Integer.MIN_VALUE);
+        System.out.println("Max value Integer " + Integer.MAX_VALUE);
+        System.out.println("#################################");
+        System.out.println("Min value Long " + Long.MIN_VALUE);
+        System.out.println("Max value Long " + Long.MAX_VALUE);
+        System.out.println("################################");
+        System.out.println("Min value Float " + Float.MIN_VALUE);
+        System.out.println("Max value Float " + Float.MAX_VALUE);
+        System.out.println("################################");
+        System.out.println("Min value Double " + Double.MIN_VALUE);
+        System.out.println("Max value Double " + Double.MAX_VALUE);
     }
 }
