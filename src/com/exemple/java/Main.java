32d5f28b34bf6444e7720fb5a8ec240d111a2877
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-10 17:22:35 +0100

    Character Type

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -3,21 +3,20 @@ package com.exemple.java;
 public class Main {
     static boolean anonymous;
     public static void main(String[] args) {
-        System.out.println("Boolean Type:");
+        System.out.println("Character Type:");
+        char firstChar = 'a';
+        char secondChar = 'b';
+        char thirdChar = 'c';
+        System.out.println("firstChar is: " + firstChar);
+        System.out.println("secondChar is: " + secondChar);
+        System.out.println("thirdChar is: " + thirdChar);
 
+        char character = '\u00c6'; //Æ
+        System.out.println("character is: " + character);
 
-        boolean bool1 = true;
-        System.out.println("Boolean is: " + bool1);
-
-        boolean bool2 = false;
-        System.out.println("Boolean is: " + bool2);
-
-        System.out.println("Boolean Type: " + anonymous);
-
-        String sBoolean = "false";
-        boolean bBoolean = Boolean.parseBoolean(sBoolean);
-        System.out.println("convert boolean from string to boolean type: " + bBoolean);
-
-
+        System.out.println("Check this out:");
+        System.out.print(Character.toUpperCase(firstChar));
+        System.out.print(Character.toUpperCase(secondChar));
+        System.out.println(Character.toUpperCase(thirdChar));
     }
 }
