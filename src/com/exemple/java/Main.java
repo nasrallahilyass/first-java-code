9a6f337e684c3aaf7e345bdfb05632edd3527e7e
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-10 18:07:52 +0100

    Arrays type: Array sort && Array copy

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -1,22 +1,38 @@
 package com.exemple.java;
 
+import java.util.Arrays;
+
 public class Main {
     static boolean anonymous;
     public static void main(String[] args) {
-        System.out.println("Character Type:");
-        char firstChar = 'a';
-        char secondChar = 'b';
-        char thirdChar = 'c';
-        System.out.println("firstChar is: " + firstChar);
-        System.out.println("secondChar is: " + secondChar);
-        System.out.println("thirdChar is: " + thirdChar);
+        System.out.println("Arrays Type:");
+        // Arrays and Array sort
+        // fist way
+        int[] numbers = {88, 1, 45, 33, 76, 3, 7};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        System.out.println("SORTED Element of numbers array:");
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        // second way
+        char[] arrayOfNumbers = {'f', 'b', 'e', 'c'};
+        System.out.println("the length of the array is: " + arrayOfNumbers.length);
+        System.out.println("the first element of the array is: " + arrayOfNumbers[0]);
+        System.out.println("SORTED Element of arrayOfNumbers is: ");
+        Arrays.sort(arrayOfNumbers);
+        for (int i = 0; i < arrayOfNumbers.length; i++) {
+            System.out.println(arrayOfNumbers[i]);
+        }
+        // Array copy
+        int[] original = {12, 3, 6, 81, 2};
+        int[] copy = new int[5];
+        System.arraycopy(original, 0, copy, 0, 5);
 
-        char character = '\u00c6'; //Æ
-        System.out.println("character is: " + character);
-
-        System.out.println("Check this out:");
-        System.out.print(Character.toUpperCase(firstChar));
-        System.out.print(Character.toUpperCase(secondChar));
-        System.out.println(Character.toUpperCase(thirdChar));
+        for (int i = 0; i < copy.length; i++) {
+            System.out.println(copy[i]);
+        }
     }
 }
