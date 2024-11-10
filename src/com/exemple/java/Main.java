5e3d2dec230666c336980a46cc06020aa2b56cc5
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-10 18:57:51 +0100

    Ternary operator && Switch case

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -2,21 +2,28 @@ package com.exemple.java;
 
 
 public class Main {
+    public static boolean userCar = true;
+
     public static void main(String[] args) {
-        System.out.println("Postfix && Prefix");
-        int postfixNumber = 10;
-        int prefixNumber = 20;
-        System.out.println("Postfix of number is: " + postfixNumber++);
-        System.out.println("Postfix of number is: " + ++prefixNumber);
+        System.out.println("Ternary operator:");
+        int userAge = 18;
+        String msg = (userAge >= 18 && userCar) ? "Allowed to drive" : "Not allowed to drive";
+        System.out.println(msg);
+        System.out.println("Switch case:");
+        int grade = 1;
+        switch (grade) {
+            case 1:
+                System.out.println("You are Excellent!");
+                break;
+            case 2:
+                System.out.println("You are Good!");
+                break;
+            case 3:
+                System.out.println("You are Bad!");
+                break;
+            default:
+                System.out.println("Please enter a grad!");
+        }
 
-        System.out.println("Current value of postfix is: " + postfixNumber);
-        System.out.println("Current value of prefix is: " + prefixNumber);
-
-        /*
-            Postfix of number is: 10
-            Postfix of number is: 21
-            Current value of postfix is: 11
-            Current value of prefix is: 21
-         */
     }
 }
