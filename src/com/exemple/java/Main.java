bb996130d262cb7dd2569cd7d7220dccbfeb40eb
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-10 18:21:15 +0100

    Postfix && Prefix

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -1,38 +1,22 @@
 package com.exemple.java;
 
-import java.util.Arrays;
 
 public class Main {
-    static boolean anonymous;
     public static void main(String[] args) {
-        System.out.println("Arrays Type:");
-        // Arrays and Array sort
-        // fist way
-        int[] numbers = {88, 1, 45, 33, 76, 3, 7};
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
-        System.out.println("SORTED Element of numbers array:");
-        Arrays.sort(numbers);
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
-        // second way
-        char[] arrayOfNumbers = {'f', 'b', 'e', 'c'};
-        System.out.println("the length of the array is: " + arrayOfNumbers.length);
-        System.out.println("the first element of the array is: " + arrayOfNumbers[0]);
-        System.out.println("SORTED Element of arrayOfNumbers is: ");
-        Arrays.sort(arrayOfNumbers);
-        for (int i = 0; i < arrayOfNumbers.length; i++) {
-            System.out.println(arrayOfNumbers[i]);
-        }
-        // Array copy
-        int[] original = {12, 3, 6, 81, 2};
-        int[] copy = new int[5];
-        System.arraycopy(original, 0, copy, 0, 5);
+        System.out.println("Postfix && Prefix");
+        int postfixNumber = 10;
+        int prefixNumber = 20;
+        System.out.println("Postfix of number is: " + postfixNumber++);
+        System.out.println("Postfix of number is: " + ++prefixNumber);
 
-        for (int i = 0; i < copy.length; i++) {
-            System.out.println(copy[i]);
-        }
+        System.out.println("Current value of postfix is: " + postfixNumber);
+        System.out.println("Current value of prefix is: " + prefixNumber);
+
+        /*
+            Postfix of number is: 10
+            Postfix of number is: 21
+            Current value of postfix is: 11
+            Current value of prefix is: 21
+         */
     }
 }
