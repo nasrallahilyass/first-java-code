91dc1325073d8da9f4933a111816ca0eadd23368
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-11 11:15:15 +0100

    do while loop and for each loop

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -3,18 +3,25 @@ package com.exemple.java;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 1;
-        while (i <= 10) {
-//            if (i == 5) {
-//                break;
-//            }
-            if (i == 5) {
-                i++;
-                continue;
-            }
-            System.out.println("This is number:" + i);
-            i++;
+        int age = 17;
+        int pointer = 1;
+        // Do While loop:
+        /*  Use while if you :
+                want the loop to run only if the condition is true from the beginning.
+            Use do-while if you:
+                need the loop to run at least once, regardless of the condition.
+        */
+        do {
+            System.out.println("You're successfully tests are: " + pointer);
+            pointer++;
         }
-        System.out.println("Loop has been finished");
+        while (pointer <= 3 && age >= 18);
+        // For each loop:
+        int[] numbers = {13, 1, 54, 6, 77};
+        for (int number : numbers) {
+            System.out.println("number is: " + number);
+        }
     }
+
+    ;
 }
