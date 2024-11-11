beeff7fb3ab9c5fa7223c8dd9ea601b587e23ef0
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-11 12:06:37 +0100

    String Type: Convert primitive to string && StringBuilder

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -1,24 +1,45 @@
 package com.exemple.java;
 
 
+import java.text.NumberFormat;
+
 public class Main {
     public static void main(String[] args) {
-        String str1 = "This is a string";
-        System.out.println(str1);
-        String str2 = "This is an other string";
-        System.out.println(str2);
-        String str3 = "My name is ";
-        String str4 = "ILYASS, i'm ";
-        String str5 = str3 + str4 + 24 + " years old.";
-        System.out.println(str5);
-        System.out.println("##################################");
-        char[] chars = {'I', 'L', 'Y', 'A', 'S', 'S'};
-        String str6 = new String(chars);
-        System.out.println(str6);
-        System.out.println("##################################");
-        char[] toCharArray = str6.toCharArray();
-        for (char c : toCharArray) {
-            System.out.println(c);
-        }
+        System.out.println(" Convert primitive to string ");
+        boolean b = true;
+        int num = 2000;
+        long longNum = 77_388_362; // just to make it simple to read
+
+        String fromBoolean = Boolean.toString(b);
+        String fromNumber = Integer.toString(num);
+        String fromLong = Long.toString(longNum);
+
+        System.out.println(
+                "Here is the Convert primitive: "
+                        + " " + fromBoolean
+                        + " " + fromNumber
+                        + " " + fromLong
+        );
+
+        System.out.println("###########################");
+        NumberFormat formatter = NumberFormat.getNumberInstance();
+        String fromLongToFormat = formatter.format(longNum);
+        System.out.println(fromLongToFormat);// 77,388,362
+        System.out.println("###########################");
+        System.out.println("StringBuilder: ");
+        /*
+        StringBuilder in Java is used for efficient,
+        mutable string manipulation,
+        especially in scenarios with frequent modifications or concatenations,
+        avoiding the memory overhead of creating multiple String objects.
+         */
+        StringBuilder stringBuilder = new StringBuilder("Hello");
+        stringBuilder.append(", ");
+        stringBuilder.append("My name is: ");
+        stringBuilder.append("Ilyass ");
+        stringBuilder.append("i'm Moroccan.");
+        System.out.println(stringBuilder);
+        stringBuilder.delete(stringBuilder.length() - 9, stringBuilder.length());
+        System.out.println(stringBuilder);
     }
 }
