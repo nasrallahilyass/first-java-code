2d5faec10504e0a8652df796b49823c360107278
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-11 12:43:10 +0100

    String Type: Comparing strings equals && equalsIgnoreCase

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -2,44 +2,19 @@ package com.exemple.java;
 
 
 import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Convert primitive to string ");
-        boolean b = true;
-        int num = 2000;
-        long longNum = 77_388_362; // just to make it simple to read
-
-        String fromBoolean = Boolean.toString(b);
-        String fromNumber = Integer.toString(num);
-        String fromLong = Long.toString(longNum);
-
-        System.out.println(
-                "Here is the Convert primitive: "
-                        + " " + fromBoolean
-                        + " " + fromNumber
-                        + " " + fromLong
-        );
-
-        System.out.println("###########################");
-        NumberFormat formatter = NumberFormat.getNumberInstance();
-        String fromLongToFormat = formatter.format(longNum);
-        System.out.println(fromLongToFormat);// 77,388,362
-        System.out.println("###########################");
-        System.out.println("StringBuilder: ");
-        /*
-        StringBuilder in Java is used for efficient,
-        mutable string manipulation,
-        especially in scenarios with frequent modifications or concatenations,
-        avoiding the memory overhead of creating multiple String objects.
-         */
-        StringBuilder stringBuilder = new StringBuilder("Hello");
-        stringBuilder.append(", ");
-        stringBuilder.append("My name is: ");
-        stringBuilder.append("Ilyass ");
-        stringBuilder.append("i'm Moroccan.");
-        System.out.println(stringBuilder);
-        stringBuilder.delete(stringBuilder.length() - 9, stringBuilder.length());
-        System.out.println(stringBuilder);
+//        System.out.print("Please Enter your name: ");
+//        Scanner sc = new Scanner(System.in);
+//        String name = sc.nextLine();
+//        System.out.println("Hello " + name);
+        String str1 = "Hi ";
+        str1 = "H";
+        String str2 = "Hi ";
+        String str3 = "HI ";
+        System.out.println(str1.equals(str2));
+        System.out.println(str2.equalsIgnoreCase(str3));
     }
 }
