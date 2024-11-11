19d04c79e85686cbf1b17f4181ee046acca258b5
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-11 12:54:31 +0100

    Formatting numeric values and Local

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -2,19 +2,16 @@ package com.exemple.java;
 
 
 import java.text.NumberFormat;
-import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.print("Please Enter your name: ");
-//        Scanner sc = new Scanner(System.in);
-//        String name = sc.nextLine();
-//        System.out.println("Hello " + name);
-        String str1 = "Hi ";
-        str1 = "H";
-        String str2 = "Hi ";
-        String str3 = "HI ";
-        System.out.println(str1.equals(str2));
-        System.out.println(str2.equalsIgnoreCase(str3));
+        Locale locale = new Locale("en", "US");
+        double number = 10090201;
+        NumberFormat formatter = NumberFormat.getNumberInstance(locale);
+        System.out.println(formatter.format(number));
+        System.out.println("#############################");
+        NumberFormat currFormatter = NumberFormat.getCurrencyInstance(locale);
+        System.out.println("currency: " + currFormatter.format(number));
     }
 }
