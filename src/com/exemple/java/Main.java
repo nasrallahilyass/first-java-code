b39d10845ca7307e76e444f92c9100cb8c1c6edc
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-12 20:47:36 +0100

    OOP: Fields && Fields, Instantiation, Anonymous Object, Access Modifiers

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -2,87 +2,27 @@ package com.exemple.java;
 
 
 public class Main {
-    // Main
     public static void main(String[] args) {
-        System.out.println("###########################");
-        Purchase(100, 0.2, "USD");
-        System.out.println("###########################");
+        Phone phone = new Phone(); // Instantiation
+
+        phone.brand = "iphone";
+        phone.os = "ios";
+        phone.model = "iphone 16 pro max";
+        phone.color = "red";
+
+        phone.Ring();
+        phone.Video();
+
+        // Anonymous Object
+        // An anonymous object is created without being assigned a reference.
+        new Phone().Ring();
+
+        // Access Modifiers
         /*
-        System.out.println(Sum(10, 10));
-        System.out.println(Diff(10, 2));
-        System.out.println(Div(10, 2));
-        System.out.println(Div(10, 0));
-        System.out.println(Mul(5, 10));
-        System.out.println(Squ(5));
+        public: Accessible from any other class.
+        protected: Accessible within the same package and by subclasses.
+        default (no modifier): Accessible only within the same package.
+        private: Accessible only within the declared class.
          */
-        System.out.println("###########################");
-        Surface(4); // Square surface
-        Surface(4, 5); // Triangle surface
-
     }
-
-    // Calculate Price:
-    public static void Purchase(int price, double tax, String currency) {
-        System.out.println("Price: " + price + " || Tax: " + tax + " || Currency: " + currency);
-        double taxIs = price * tax;
-        System.out.println("Tax is: " + taxIs);
-        double finalPrice = price + taxIs;
-        System.out.println("Final Price: " + finalPrice + " " + currency);
-        ConvertCurrency(currency, finalPrice);
-    }
-
-    public static void ConvertCurrency(String currency, double finalPrice) {
-        switch (currency) {
-            case "USD":
-                System.out.println("To MAD: " + finalPrice * 10 + "MAD");
-                break;
-            case "EUR":
-                System.out.println("To MAD: " + finalPrice * 11 + "MAD");
-                break;
-        }
-    }
-
-    // Operation:
-    public static double Sum(int num1, int num2) {
-        return num1 + num2;
-    }
-
-    public static double Diff(int num1, int num2) {
-        return num1 - num2;
-    }
-
-    public static int Mul(int num1, int num2) {
-        return num1 * num2;
-    }
-
-    public static double Squ(int num1) {
-        return Math.pow(num1, 2);
-    }
-
-    public static double Div(int num1, int num2) {
-        try {
-            return (double) num1 / num2;
-        } catch (ArithmeticException e) {
-            System.out.println("Error: Division by zero is not allowed.");
-        }
-        return 0;
-    }
-
-
-    // Methods overloading:
-
-    // Square:
-    public static void Surface(int num) {
-        int surface;
-        surface = num * num;
-        System.out.println("Surface of Square is : " + surface);
-    }
-
-    // Rectangle:
-    public static void Surface(int length, int width) {
-        int surface;
-        surface = length * width;
-        System.out.println("Surface of Rectangle is : " + surface);
-    }
-
 }
---
+++ src/com/exemple/java/Phone.java
@@ -0,0 +1,19 @@
+package com.exemple.java;
+
+public class Phone {
+
+    // Fields:
+    public String os;
+    public String model;
+    public String brand;
+    public String color;
+
+    // Methods:
+    public void Ring() {
+        System.out.println("Ring...");
+    }
+
+    public void Video() {
+        System.out.println("Filming a Video...");
+    }
+}
