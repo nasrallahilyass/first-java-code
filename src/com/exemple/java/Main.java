08d200158ee5d2172dce0ccf9911a0611359cf57
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-12 12:05:37 +0100

    Debugging tool && Try / Catch

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -1,23 +1,17 @@
 package com.exemple.java;
 
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("LocalDateTime && DateTimeFormatter:");
-        LocalDateTime date = LocalDateTime.now();
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ISO_DATE;
-        System.out.println("##########################");
-        System.out.println(dateFormatter.format(date));
-        System.out.println("##########################");
-        DateTimeFormatter dateFormatterPattern = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        System.out.println(dateFormatterPattern.format(date));
-        System.out.println("##########################");
-
-
+        String str = "Hello!";
+        char[] chars = str.toCharArray();
+        try {
+            for (int i = 0; i <= chars.length; i++) {
+                System.out.println(chars[i]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Finished!");
     }
 }
