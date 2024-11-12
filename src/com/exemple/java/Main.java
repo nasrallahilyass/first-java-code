06574415ea116e1d4777a88d50de8e8a55272852
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-12 12:25:01 +0100

    throw and Multiple catch blocks

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -3,14 +3,27 @@ package com.exemple.java;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "Hello!";
-        char[] chars = str.toCharArray();
         try {
-            for (int i = 0; i <= chars.length; i++) {
-                System.out.println(chars[i]);
+            String sAge = "102";
+            int age = Integer.parseInt(sAge);
+            if (age > 100) {
+                throw (new Exception("You're dead"));
             }
+            System.out.println("The age is " + age);
+            String s = "Hello! I'm " + age + " years old";
+
+            char[] chars = s.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                System.out.print(chars[i]);
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("index not found");
+        } catch (NumberFormatException e) {
+            System.out.println("Number can't be converted to int");
         } catch (Exception e) {
-            e.printStackTrace();
+//            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
         System.out.println("Finished!");
     }
