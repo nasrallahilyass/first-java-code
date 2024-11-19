6428a9524dddc268c7c040f6ef6625fe615b0294
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-19 14:34:35 +0100

    File Handling: FileOutputStream class

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -1,30 +1,20 @@
 package com.exemple.java;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        FileInputStream fis = new FileInputStream("test.txt");
-        System.out.println("########## FileInputStream ##########");
-        System.out.println("First way to read the file: ");
-        System.out.println("Print the whole file: ");
+        System.out.println("########## FileOutputStream ##########");
+        FileOutputStream fos = new FileOutputStream("check.txt");
+        fos.write("Hello World".getBytes());
+        fos.flush(); // fix the issue of not writing the data to the file
+        FileInputStream fis = new FileInputStream("check.txt");
         int data;
         while ((data = fis.read()) != -1) {
-            System.out.println((char) data);
+            System.out.print((char) data);
         }
-        fis.close();
-        System.out.println("Second way to read the file: ");
-        File file = new File("test.txt");
-        FileInputStream fis2 = new FileInputStream(file);
-        byte[] byteArray = new byte[(int) file.length()];
-        fis2.read(byteArray);
-        String data2 = new String(byteArray);
-        System.out.println(data2);
-        fis2.close();
+
 
     }
 }
