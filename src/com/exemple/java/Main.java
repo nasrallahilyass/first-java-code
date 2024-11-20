4259fcedcc65a3287cf8687f17207c28aa1a80b4
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-20 12:37:07 +0100

    File Handling (PrintWriter class "Constructors & Methods")

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -6,23 +6,13 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.println("########## File ##########");
-        FileWriter fw = new FileWriter("file.txt");
-        System.out.println("########## FileWriter ##########");
-        BufferedWriter bw = new BufferedWriter(fw);
-        bw.write("Hello World");
-        bw.newLine();
-        bw.write("That was super easy !");
-        bw.flush();
-        bw.close();
-
-        System.out.println("########## FileReader ##########");
-        FileReader fr = new FileReader("file.txt");
-        BufferedReader br = new BufferedReader(fr);
-        String line = br.readLine();
-        while (line != null) {
-            System.out.println(line);
-            line = br.readLine(); // read next line
-        }
+        FileWriter file = new FileWriter("file.txt", true);
+        PrintWriter writer = new PrintWriter(file);
+        writer.println("Hello, World!");
+        writer.println(2.81);
+        writer.print(false);
+        writer.flush();
+        writer.close();
     }
 }
 
