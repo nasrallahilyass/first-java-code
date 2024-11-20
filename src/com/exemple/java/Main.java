eb0785f5d5a92414344b976903f014d4c7337f99
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-20 12:04:26 +0100

    File Handling (File class):File Handling (Reader class/Writer class hierarchy)

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -6,59 +6,9 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.println("########## File ##########");
-        /*
-        File file = new File("file.txt");
-        System.out.println("Is file exists: " + file.exists());
-        file.createNewFile();
-        if (file.exists()) {
-            System.out.println("File name: " + file.getName());
-            System.out.println("File path: " + file.getAbsolutePath());
-            System.out.println("File writeable: " + file.canWrite());
-            System.out.println("File readable: " + file.canRead());
-            System.out.println("File size in bytes: " + file.length());
-            System.out.println("Is directory: " + file.isDirectory());
-            System.out.println("Is file: " + file.isFile());
-            System.out.println("Is hidden: " + file.isHidden());
-            System.out.println("Last modified: " + file.lastModified());
-            System.out.println("Can execute: " + file.canExecute());
-        }
-
-         */
-        File file = new File("Directory");
-        file.mkdir();
-        /*
-        file.mkdirs(); //if you want to create multiple directories File file = new File("Directory1/Directory2/Directory3");
-        file.delete();
-         */
-        String[] elements = file.list();
-
-        //Constructors
-        File file1 = new File("Directory", "file.txt");
-        file1.createNewFile();
-        File file2 = new File("Directory/Docs", "Microsoft Word Document.docx");
-        file2.createNewFile();
-
-        //List files in directory
-        if (file.isDirectory()) {
-            for (String element : elements) {
-                System.out.println(element);
-            }
-        }
-
-        // Write to file
-        FileOutputStream fos = new FileOutputStream(file2);
-        fos.write("Hello World!".getBytes());
-        fos.flush();
-        fos.close();
-
-        // Read from file
-        System.out.println("########## Read from file ##########");
-        FileInputStream fis = new FileInputStream(file2);
-        int data;
-        while ((data = fis.read()) != -1) {
-            System.out.print((char) data);
-        }
-        fis.close();
+        FileWriter file = new FileWriter("file.txt");
+        file.write("Wow! That was easy to write :)");
+        file.close();
     }
 }
 
