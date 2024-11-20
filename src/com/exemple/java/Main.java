0788c50ba678d91959cf77869e52e0df810d54b7
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-20 12:23:54 +0100

    File Handling (BufferedReader / BufferedWriter classes, Read file line by line)

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -6,9 +6,23 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.println("########## File ##########");
-        FileWriter file = new FileWriter("file.txt");
-        file.write("Wow! That was easy to write :)");
-        file.close();
+        FileWriter fw = new FileWriter("file.txt");
+        System.out.println("########## FileWriter ##########");
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("Hello World");
+        bw.newLine();
+        bw.write("That was super easy !");
+        bw.flush();
+        bw.close();
+
+        System.out.println("########## FileReader ##########");
+        FileReader fr = new FileReader("file.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine();
+        while (line != null) {
+            System.out.println(line);
+            line = br.readLine(); // read next line
+        }
     }
 }
 
