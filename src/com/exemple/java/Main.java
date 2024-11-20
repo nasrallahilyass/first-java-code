966b1ceaf2bc9e5d16c485b32a867bd384fed851
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-20 11:39:53 +0100

    File Handling (File class): Add/remove && constructor.

--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -6,6 +6,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.println("########## File ##########");
+        /*
         File file = new File("file.txt");
         System.out.println("Is file exists: " + file.exists());
         file.createNewFile();
@@ -21,6 +22,43 @@ public class Main {
             System.out.println("Last modified: " + file.lastModified());
             System.out.println("Can execute: " + file.canExecute());
         }
+
+         */
+        File file = new File("Directory");
+        file.mkdir();
+        /*
+        file.mkdirs(); //if you want to create multiple directories File file = new File("Directory1/Directory2/Directory3");
+        file.delete();
+         */
+        String[] elements = file.list();
+
+        //Constructors
+        File file1 = new File("Directory", "file.txt");
+        file1.createNewFile();
+        File file2 = new File("Directory/Docs", "Microsoft Word Document.docx");
+        file2.createNewFile();
+
+        //List files in directory
+        if (file.isDirectory()) {
+            for (String element : elements) {
+                System.out.println(element);
+            }
+        }
+
+        // Write to file
+        FileOutputStream fos = new FileOutputStream(file2);
+        fos.write("Hello World!".getBytes());
+        fos.flush();
+        fos.close();
+
+        // Read from file
+        System.out.println("########## Read from file ##########");
+        FileInputStream fis = new FileInputStream(file2);
+        int data;
+        while ((data = fis.read()) != -1) {
+            System.out.print((char) data);
+        }
+        fis.close();
     }
 }
 
