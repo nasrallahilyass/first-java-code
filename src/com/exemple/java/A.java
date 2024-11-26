b9f5577a332582d7a6c3457f2eb134d4ea067746
nasrallahilyass <nasrallah.dev@gmail.com>
2024-11-26 11:34:46 +0100

    ExecutorService

--- src/com/exemple/java/A.java
+++ src/com/exemple/java/A.java
@@ -1,22 +1,15 @@
 package com.exemple.java;
 
-public class A {
+public class A implements Runnable {
 
-    Names names = new Names();
 
-    public void work1() throws InterruptedException {
-        synchronized (names) {
-            System.out.println("Enter your name: ");
-            names.wait();
-            System.out.println(names.name);
-        }
-    }
-
-    public void work2() throws InterruptedException {
-        synchronized (names) {
-            Thread.sleep(500);
-            names.name = Thread.currentThread().getName();
-            names.notify();
+    @Override
+    public void run() {
+        try {
+            System.out.println("Thread " + Thread.currentThread().getName() + " is running");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/com/exemple/java/Main.java
+++ src/com/exemple/java/Main.java
@@ -1,35 +1,27 @@
 package com.exemple.java;
 
+import java.util.ArrayList;
+import java.util.concurrent.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(2);
         A a = new A();
-        Thread t1 = new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            a.work1();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-        );
-        Thread t2 = new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            a.work2();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-        );
+        Thread t1 = new Thread(a);
+        Thread t2 = new Thread(a);
+        Thread t3 = new Thread(a);
+        Thread t4 = new Thread(a);
 
-        t1.start();
-        t2.start();
+        ArrayList<Thread> threads = new ArrayList<>();
+        threads.add(t1);
+        threads.add(t2);
+        threads.add(t3);
+        threads.add(t4);
+
+        for (Thread t : threads) {
+            es.execute(t);
+        }
+
     }
 }
